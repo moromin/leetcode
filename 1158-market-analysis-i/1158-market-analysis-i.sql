0e216bcb7ec80fab9097ff97ff96e618abd42be7
moromin <34294957+moromin@users.noreply.github.com>
2022-07-06 11:55:46 +0900

    refactor: use sum and case

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,11 +1,16 @@
 # Write your MySQL query statement below
 select
     u.user_id as buyer_id,
-    u.join_date,
-    count(order_date) as orders_in_2019
+    u.join_date, 
+    sum(
+        case
+            when YEAR(o.order_date) = 2019 then 1
+            else 0
+        end
+    ) as orders_in_2019
 from Users as u
-left join Orders as o
-on u.user_id = o.buyer_id and YEAR(o.order_date) = '2019'
+left outer join Orders as o
+on u.user_id = o.buyer_id
 group by u.user_id
 
 # SELECT
