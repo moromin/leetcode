7926b67342c0a743e694b2f004d33d1092ac4c7a
moromin <34294957+moromin@users.noreply.github.com>
2022-07-08 16:51:11 +0900

    Time: 1639 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 1571-warehouse-manager/1571-warehouse-manager.sql
+++ 1571-warehouse-manager/1571-warehouse-manager.sql
@@ -1,12 +1,8 @@
 # Write your MySQL query statement below
 select
-    # *
-    w.name as warehouse_name, 
-    sum(w.units * p.volume) as volume
+    name as warehouse_name, 
+    sum(units * Width * Length * Height) as volume
 from Warehouse as w
-join (
-    select product_id, Width * Length * Height as volume
-    from Products
-) as p
+join Products as p
 on w.product_id = p.product_id
-group by w.name;
+group by name;
