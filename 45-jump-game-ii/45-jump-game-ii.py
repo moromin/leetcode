b406b5152d0f1c87c2197e49938c1034a64bb90f
moromin <34294957+moromin@users.noreply.github.com>
2022-07-19 16:22:51 +0900

    Time: 225 ms (56.00%), Space: 15 MB (89.54%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -3,8 +3,8 @@ class Solution:
         ans = lastIdx = nextIdx = 0
         while nextIdx < len(nums) - 1:
             ans += 1
-            print(lastIdx, nextIdx)
+            # print(lastIdx, nextIdx)
             lastIdx, nextIdx = nextIdx, max(i + nums[i] for i in range(lastIdx, nextIdx + 1))
-            print(lastIdx, nextIdx)
-            print('---------------')
+            # print(lastIdx, nextIdx)
+            # print('---------------')
         return ans
