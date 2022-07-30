54e36360d84dff7903225ce7caf13d1af49e1963
moromin <34294957+moromin@users.noreply.github.com>
2022-07-30 15:00:50 +0900

    Time: 2823 ms (5.01%), Space: 18.5 MB (73.00%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -1,11 +1,25 @@
 from collections import Counter
 
 class Solution:
-    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        count = Counter()
-        for word in words2:
-            count |= Counter(word)
+#     Counter solution
+#     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+#         count = Counter()
+#         for word in words2:
+#             count |= Counter(word)
         
-        return [word for word in words1 if not count - Counter(word)]
+#         return [word for word in words1 if not count - Counter(word)]
             
-            
+#   Map solution
+    def wordSubsets(self, words1, words2):
+        def count(word):
+            ans = [0 for _ in range(26)]
+            for c in word:
+                ans[ord(c) - ord('a')] += 1
+            return ans
+    
+        bmax = [0 for _ in range(26)]
+        for word in words2:
+            for i, c in enumerate(count(word)):
+                bmax[i] = max(bmax[i], c)
+        
+        return [word for word in words1 if all(x >= y for x, y in zip(count(word), bmax))]
