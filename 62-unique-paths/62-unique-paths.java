94266d07c217c90eec3d4c5a50e24f35d3e7a149
moromin <34294957+moromin@users.noreply.github.com>
2022-08-01 11:16:20 +0900

    Time: 0 ms (100.00%), Space: 38.9 MB (98.27%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,13 +1,4 @@
 class Solution {
-    public void printArray(int[][] array) {
-        for(int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                System.out.print(array[i][j]);
-            }
-            System.out.println("");
-        }    
-    }
-    
     public int uniquePaths(int m, int n) {
         int[][] grid = new int[m][n];
         for (int i = 0; i < m; i++) {
