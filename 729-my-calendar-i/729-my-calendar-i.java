9db039c12714f122d0f5518da625aa0ab75376b0
moromin <34294957+moromin@users.noreply.github.com>
2022-08-03 14:36:02 +0900

    Time: 38 ms (55.63%), Space: 54.5 MB (51.82%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,18 +1,33 @@
 class MyCalendar {
-    List<int[]> books;
+//     List<int[]> books;
 
+//     public MyCalendar() {
+//         this.books = new ArrayList<int[]>();
+//     }
+    
+//     public boolean book(int start, int end) {
+//         for (int[] book: this.books) {
+//             // System.out.println(book[0] + " " + book[1]);
+//             if (!(book[1] <= start || end <= book[0])) 
+//                 return false;
+//         }
+//         this.books.add(new int[]{start, end});
+//         return true;
+//     }
+    
+    TreeMap<Integer, Integer> calendar = new TreeMap<>();
+    
     public MyCalendar() {
-        this.books = new ArrayList<int[]>();
+        this.calendar.put(Integer.MAX_VALUE, Integer.MAX_VALUE);
     }
     
     public boolean book(int start, int end) {
-        for (int[] book: this.books) {
-            // System.out.println(book[0] + " " + book[1]);
-            if (!(book[1] <= start || end <= book[0])) 
-                return false;
-        }
-        this.books.add(new int[]{start, end});
-        return true;
+        Map.Entry<Integer, Integer> pair = this.calendar.higherEntry(start);
+        // System.out.println(pair.getKey() + " " + pair.getValue());
+        boolean res = end <= pair.getValue();
+        if (res)
+            this.calendar.put(end, start);
+        return res;
     }
 }
 
