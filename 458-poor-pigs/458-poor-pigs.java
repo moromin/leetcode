4e8c32cc4bfa63ad81e5b1364ed7e86e187e1bc4
moromin <34294957+moromin@users.noreply.github.com>
2022-08-06 13:05:00 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (79.20%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.java
+++ 458-poor-pigs/458-poor-pigs.java
@@ -2,9 +2,13 @@ class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
         int rounds = minutesToTest / minutesToDie;
         int pigs = 0;
+        int N = 1;
         
-        while (Math.pow((rounds + 1), pigs) < buckets)
-            pigs++;
+        while (N < buckets)
+        {
+            N *= (rounds + 1);
+            pigs++;   
+        }
         return pigs;
     }
 }
