7e7b76f4b3e5db6f7437bc49771d2dd3fa0cc294
moromin <34294957+moromin@users.noreply.github.com>
2022-08-12 14:40:16 +0900

    Time: 109 ms (65.63%), Space: 18.8 MB (68.62%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,30 +6,12 @@
 #         self.right = None
 
 class Solution:
-    def traverse(self, node, parents):
-        if node is None:
-            return parents
-        if node.left is not None:
-            parents[node.left] = node
-            self.traverse(node.left, parents)
-        if node.right is not None:
-            parents[node.right] = node
-            self.traverse(node.right, parents)
-        return parents
     
     def lowestCommonAncestor(self, root, p, q):
-        parents = {root: None}
-        parents = self.traverse(root, parents)
-        
-        ancestors = set()
-        parent = p
-        
-        while parent is not None:
-            ancestors.add(parent)
-            parent = parents[parent]
-        
-        node = q
-        while node not in ancestors:
-            node = parents[node]
-        
-        return node
+        while True:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
