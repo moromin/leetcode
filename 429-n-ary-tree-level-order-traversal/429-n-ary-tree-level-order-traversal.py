73baa96fc4090f337bff8d7e74685ab48f2616e4
moromin <34294957+moromin@users.noreply.github.com>
2022-09-05 10:18:47 +0900

    Time: 114 ms (8.28%), Space: 16 MB (50.08%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
@@ -7,24 +7,47 @@ class Node:
 """
 
 class Solution:
-    def levelOrder(self, root: 'Node') -> List[List[int]]:
+#     queue
+#     def levelOrder(self, root: 'Node') -> List[List[int]]:
+#         if not root:
+#             return []
+        
+#         res = []
+#         q = collections.deque()
+#         q.append(root)
+#         while q:
+#             tmp = []
+#             for i in range(len(q)):
+#                 node = q.popleft()
+#                 if node:
+#                     tmp.append(node.val)
+#                     for child in node.children:
+#                         q.append(child)
+#             if tmp:
+#                 res.append(tmp)
+#         return res
+
+    # recursion
+    def levelOrder(self, root):
         if not root:
             return []
         
         res = []
-        q = collections.deque()
-        q.append(root)
-        while q:
-            tmp = []
-            for i in range(len(q)):
-                node = q.popleft()
-                if node:
-                    tmp.append(node.val)
-                    for child in node.children:
-                        q.append(child)
-            if tmp:
-                res.append(tmp)
+        
+        def bfs(nodes):
+            if not nodes:
+                return
+            
+            children, nexts = [], []
+            for node in nodes:
+                children.append(node.val)
+                nexts += node.children
+            res.append(children)
+            bfs(nexts)
+        
+        bfs([root])
         return res
+    
             
         
         
