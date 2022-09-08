5b25d15aa36ba8769e8cc7fa55d49c68cd6d47b9
moromin <34294957+moromin@users.noreply.github.com>
2022-09-09 05:30:45 +0900

    Time: 10 ms (43.21%), Space: 67.7 MB (12.71%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,6 +1,9 @@
 class Solution {
     
     public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
         int m = matrix.length;
         int n = matrix[0].length;
         int result = 0;
@@ -15,21 +18,6 @@ class Solution {
                 }
             }
         }
-        
-        // System.out.println("matrix");
-        // for (int i = 0; i < m; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         System.out.print(matrix[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        // System.out.println("\ndp");
-        // for (int i = 0; i < m; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         System.out.print(dp[i+1][j+1] + " ");
-        //     }
-        //     System.out.println();
-        // }
         
         return result * result;
     }
