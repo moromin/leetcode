cba3ce13561fdc54fb95cb32c110e2e7a4211001
moromin <34294957+moromin@users.noreply.github.com>
2022-09-10 12:01:56 +0900

    Time: 209 ms (27.38%), Space: 130.9 MB (48.62%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -3,23 +3,20 @@ import java.util.*;
 class Solution {
     
     public int numberOfWeakCharacters(int[][] properties) {
-        if (properties == null || properties[0] == null) {
-            return 0;
-        }
-        
-        Arrays.sort(properties, (a, b) -> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
+        Arrays.sort(properties, (a, b) -> a[0] == b[0] ? Integer.compare(b[1], a[1]) : Integer.compare(a[0], b[0]));
         
         int res = 0;
-        Stack<Integer> stack = new Stack<>();
-        for (int[] p: properties) {
-            int atk = p[0], def = p[1];
-            while (!stack.isEmpty() && stack.peek() < def) {
-                stack.pop();
-                res++;
-            }
-            stack.push(def);
-        }
+        int len = properties.length;
         
+        int maxDef = properties[len-1][1];
+        
+        for (int i = len-2; i >= 0; i--) {
+            if (properties[i][1] < maxDef) {
+                res++;
+            } else {
+                maxDef = properties[i][1];
+            }
+        }
         return res;
     }
 }
