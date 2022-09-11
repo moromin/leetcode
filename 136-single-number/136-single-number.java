09b251a5dfa770081d7f0f7a63c69ccc0accc6fd
moromin <34294957+moromin@users.noreply.github.com>
2022-09-12 02:55:23 +0900

    Time: 2 ms (66.75%), Space: 50.8 MB (40.08%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,10 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int res = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            res ^= nums[i];
-            // System.out.println(Integer.toBinaryString(res));
-        }
+        int res = 0;
+        for (int num: nums) 
+            res ^= num;
         return res;
     }
 }
