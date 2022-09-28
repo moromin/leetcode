fc3cf702d360cbd257e4dad98bb881c40afcbdfd
moromin <34294957+moromin@users.noreply.github.com>
2022-09-28 18:47:32 +0900

    Time: 58 ms (42.72%), Space: 13.8 MB (58.87%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,21 +1,31 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits)
-        def traverse(idx) -> bool:
-            if idx == n - 1:
-                res = digits[idx] + 1
-                digits[idx] = res % 10
-                return res // 10 == 1
+# recursive
+#         n = len(digits)
+#         def traverse(idx) -> bool:
+#             if idx == n - 1:
+#                 res = digits[idx] + 1
+#                 digits[idx] = res % 10
+#                 return res // 10 == 1
             
-            res = digits[idx]
-            if traverse(idx+1):
-                res += 1
-            digits[idx] = res % 10
-            return res // 10 == 1
+#             res = digits[idx]
+#             if traverse(idx+1):
+#                 res += 1
+#             digits[idx] = res % 10
+#             return res // 10 == 1
             
-        if traverse(0):
-            digits = [1] + digits
-        return digits
+#         if traverse(0):
+#             digits = [1] + digits
+#         return digits
             
                 
-        
+# simple
+        sums = 0
+        for d in digits:
+            sums = d + sums * 10
+        sums += 1
+        res = collections.deque()
+        while sums:
+            res.appendleft(sums % 10)
+            sums //= 10
+        return list(res)
