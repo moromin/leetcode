d8011c5b287c2d2ffa8df4718254791c8d2e1741
moromin <34294957+moromin@users.noreply.github.com>
2022-11-24 23:13:44 +0900

    Time: 30 ms (96.98%), Space: 13.8 MB (63.69%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -5,18 +5,15 @@ class Solution:
         if len(stones) == 1:
             return stones[0]
         
-        stones = list(map(lambda x: x*(-1), stones)) 
+        stones = [-s for s in stones]
         heapq.heapify(stones)
         
         while len(stones) > 1:
             # print(stones)
-            x = heapq.heappop(stones) * -1
-            y = heapq.heappop(stones) * -1
+            x = heapq.heappop(stones)
+            y = heapq.heappop(stones)
             
-            if x == y:
-                continue
-            elif x < y:
-                x, y = y, x
-            heapq.heappush(stones, -(x - y))
+            if x != y:
+                heapq.heappush(stones, x - y)
             
         return -stones[0] if len(stones) == 1 else 0
