40c5fdb76202b8984135554182482ff6520dd227
moromin <34294957+moromin@users.noreply.github.com>
2024-09-17 18:50:51 +0900

    Time: 808 ms (8.18%), Space: 27.4 MB (90.68%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,8 +3,7 @@ class Solution:
         res = 0
         min_ = prices[0]
         for i in range(len(prices)):
-            if min_ > prices[i]:
-                min_ = prices[i]
+            min_ = min(min_, prices[i])
             res = max(res, prices[i] - min_)
         
         return res
